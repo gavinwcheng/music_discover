3ccb859075364fdb9b991981cc2372e7c1e74ad8
gavin.w.cheng <gavin.w.cheng@gmail.com>
2015-11-04 16:38:16 +0000

    close #12

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,11 @@
 module UsersHelper
-  def save_artists user
-    user.playlists.each do |playlist|
+  def save_artists spotify_user, database_user
+    spotify_user.playlists.each do |playlist|
       playlist.tracks.each do |track|
         track.artists.each do |artist|
           Artist.merge(name: artist.name)
+          database_artist = Artist.find_by(name: artist.name)
+          database_user.artists << database_artist unless database_user.artists.include? database_artist
         end
       end
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User
   include Neo4j::ActiveNode
   property :username, type: String
   property :email, type: String
+  has_many :out, :artists, type: :LISTENS_TO
 end
