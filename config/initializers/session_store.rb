ca07c0baf4fe60919b7c8845aa848541f07c925d
gavin.w.cheng <gavin.w.cheng@gmail.com>
2015-11-06 19:00:32 +0000

    close #74

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store, key: '_music_discover_session'
+
+MusicDiscover::Application.config.session_store :cookie_store,
+  :key => '_my_session',
+  :expire_after => 90.minutes
