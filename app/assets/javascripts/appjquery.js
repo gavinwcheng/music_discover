136b2f474fa0eb69bfc5d49d92dc29b4ac40e3e4
Dom Batten <dominic.batten@googlemail.com>
2015-11-06 17:33:16 +0000

    working on parallax

--- app/assets/javascripts/appjquery.js
+++ app/assets/javascripts/appjquery.js
@@ -35,4 +35,15 @@ $(document).ready(function() {
     opacity:1
   }, 500);
 
+  var target = $('#logo_box');
+  var targetHeight = target.outerHeight();
+
+  $(document).scroll(function(e){
+      var scrollPercent = ((targetHeight) - window.scrollY) / (targetHeight);
+      if(scrollPercent >= 0){
+          target.css('opacity', scrollPercent);
+      }
+  });
+
+
 });
