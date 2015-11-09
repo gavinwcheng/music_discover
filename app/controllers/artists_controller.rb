45dc6f01416fe54f12c6e2257377c87a1da657dc
Emily Worrall <worrall.emily@gmail.com>
2015-11-09 11:02:43 +0000

    Save button works now

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -7,16 +7,15 @@ class ArtistsController < ApplicationController
   end
 
   def playlist
-    @yooser = retrieve_spotify_user(current_user)
     recommended_users = recommend_users(current_user)
     overlapped_artists = identify_overlapped_artists(current_user)
     retrieve_info_from_spotify(recommended_users, overlapped_artists)
   end
 
   def save_track
-    user = retrieve_spotify_user(params[:userid])
+    current_spotify_user = RSpotify::User.find(current_user.username)
     track = retrieve_spotify_track(params[:trackid])
-    user.save_tracks!([track])
+    current_spotify_user.save_tracks!([track])
     redirect_to '/artists/playlist'
   end
 end
