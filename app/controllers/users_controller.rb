541d2546a6c9edfb37901fed93bad5ca2cce476d
Emily Worrall <worrall.emily@gmail.com>
2015-11-09 15:57:12 +0000

    No errors if user doesn't have image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def sync_profile
     current_spotify_user = instantiate_spotify_user(request.env['omniauth.auth'])
     current_database_user = User.merge(username: current_spotify_user.id, email: current_spotify_user.email)
-    current_database_user.image_url = current_spotify_user.images.first.url if current_spotify_user.images.first.url
+    current_database_user.image_url = current_spotify_user.images.first.url if current_spotify_user.images.first
     current_database_user.save
     session[:user_id] = current_database_user.id
     save_artists(current_spotify_user, current_database_user)
