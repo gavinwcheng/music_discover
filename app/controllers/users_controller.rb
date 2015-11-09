9ea3828d149748fc6be4dca85998658ba67fc6fa
Emily Worrall <worrall.emily@gmail.com>
2015-11-09 15:26:21 +0000

    Current user image appears on the index page when signed in:

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,14 @@ class UsersController < ApplicationController
   include RspotifyHelper
 
   def index
-      @current_spotify_user = RSpotify::User.find(current_user.username)
+
   end
 
   def sync_profile
     current_spotify_user = instantiate_spotify_user(request.env['omniauth.auth'])
     current_database_user = User.merge(username: current_spotify_user.id, email: current_spotify_user.email)
+    current_database_user.image_url = current_spotify_user.images.first.url if current_spotify_user.images.first.url
+    current_database_user.save
     session[:user_id] = current_database_user.id
     save_artists(current_spotify_user, current_database_user)
     redirect_to '/artists/match'
